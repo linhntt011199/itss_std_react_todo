879756909e9e5c797174bed5ead026f88a2b3b38
linhntt011199 <nguyenthithuylinh01111999@gmail.com>
2021-05-21 04:13:29 +0000

    Save Todo on Firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,3 +8,33 @@ const firebaseConfig = {
   appId: "1:436441747989:web:c116e095fa52f7d9c6741c"
 };
 firebase.initializeApp(firebaseConfig);
+// reference to totos collection
+const collection = firebase.firestore().collection("todos");
+
+// get items
+export const getFbItems = async () => {
+    const data = await collection.get();
+    const items = data.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+}
+
+//add item
+export const addFbItem = async (item) => {
+    await collection.add(item);
+}
+
+//update item
+export const updateFbItem = async (item, id) => {
+    await collection.doc(id).update(item);
+}
+
+//delete item
+export const deleteFbItem = async (item) => {
+  const todoRef = collection.doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+}; 
